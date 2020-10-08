9788a27758931cf6d313280a24867518912ca93c
BCA <razeshbaziko24@gmail.com>
2020-10-08 14:59:28 +0545

    add comment

---
+++ if.py
@@ -0,0 +1,25 @@
+#learning python
+'''age= int(input("Enter your age:"))
+if age > 18:
+print("You are above 18")
+elif age == 18:
+    print("You are equal to 18")
+else:
+    print("You are below 18")'''
+#single line:
+#grade= int(input("Enter your marks: "))
+#result= "pass" if grade > 40 else "fail"
+#print(result)
+
+#nested loop
+grade =int(input("Enter your marks: "))
+if grade > 40:
+    if grade >= 60:
+        if grade < 80:
+            print("First Division")
+    elif grade >= 80 and grade < 90:
+        print("Distinction")
+    else:
+        print("Second Division")
+else:
+    print("Fail")S
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 print("hello world")
 print("hello rajesh")
+Conmputer lab
